38b606b7f10148abae351b1e5fb49a142a13a949
martin <kaweesimartinanderson@gmail.com>
2019-06-17 02:06:24 -0400

    changed to alerts

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 var num=(Math.floor(Math.random() * 100) + 1);
 if (num%2==0)
-			console.log("The number is even!")
+			alert("The number is even!")
 else if (num%2==1)
 	{
-		console.log("The number is odd!");
+		alert("The number is odd!");
 		}
 for (count = 1;count<=10;count++)
 	{
 	guess= prompt("Guess a number of your choice.");
 	if (guess>num) {
-	console.log("Ooops!,its higher")
+	alert("Ooops!,its higher")
 }
 else if (guess<num) {
-	console.log("Oooh!,its lower")
+	alert("Oooh!,its lower")
 }
 
 	if (guess==num)
 	{
-		console.log("YOU WIN")
+		alert("YOU WIN")
 		break;
 		}
 	if (count==10){
-console.log("YOU LOOSE");}}
+ alert("YOU LOOSE");}}
